0ee8a74e853f037b5341969a6066f1adc7381ae7
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2026-04-02 19:35:59 +0700

    feat: create chat layout

---
+++ resources/js/Layouts/ChatLayout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+
+const ChatLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <>
+            ChatLayout
+            {children}
+        </>
+    );
+};
+
+export default ChatLayout;
