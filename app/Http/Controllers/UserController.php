8f64edb9c46b407a74c42c46bd43243a41ebb003
Phạm Hồng Kỳ <535arr@gmail.com>
2026-05-15 21:22:26 +0700

    feat(auth): implement active user middleware and enhance login error handling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -15,7 +14,7 @@ class UserController extends Controller
             'email' => ['required', 'email', 'unique:users,email'],
             'is_admin' => 'boolean',
         ]);
-        //generate random password
+        // generate random password
         // $rawPassword = Str::random(8);
         $rawPassword = 'password';
         $data['password'] = bcrypt($rawPassword);
@@ -28,21 +27,20 @@ class UserController extends Controller
 
     public function changeRole(User $user)
     {
-        $user->update(['is_admin' => !(bool) $user->is_admin]);
-        $message = "User role was changed to " . ($user->is_admin ? 'Admin' : 'Regular User');
+        $user->update(['is_admin' => ! (bool) $user->is_admin]);
+        $message = 'User role was changed to '.($user->is_admin ? 'Admin' : 'Regular User');
 
         return response()->json(['message' => $message]);
     }
 
     public function blockUnblock(User $user)
     {
-        if($user->blocked_at) {
-            $user->blocked_at = null;
-            $message = "Your account has been activated.";
-        } else {
-            $user->blocked_at = now();
-            $message = "Your account has been blocked.";
-        }
+        $blockedAt = $user->blocked_at ? null : now();
+        $user->update(['blocked_at' => $blockedAt]);
+
+        $message = $blockedAt
+            ? "User {$user->name} has been blocked."
+            : "User {$user->name} has been activated.";
 
         return response()->json(['message' => $message]);
     }
