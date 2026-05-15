788630b0ed4ab2286f1c410933e296085ead239c
Dao161004 <165215298+Dao161004@users.noreply.github.com>
2026-05-15 23:16:18 +0700

    feat: Add New Users

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -7,6 +7,9 @@ import { useState, useEffect } from 'react';
 import { useEventBus } from '../EventBus';
 import Toast from '@/Components/App/Toast';
 import NewMessageNotification from '@/Components/App/NewMessageNotification';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { UserPlusIcon } from '@heroicons/react/24/solid';
+import NewUserModal from '@/Components/App/NewUserModal';
 
 export default function AuthenticatedLayout({ header, children }) {
     const page = usePage();
@@ -14,6 +17,7 @@ export default function AuthenticatedLayout({ header, children }) {
     const conversations = page.props.conversations;
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
+    const [showNewUserModal, setShowNewUserModal] = useState(false);
     const { emit } = useEventBus();
 
     useEffect(() => {
@@ -110,7 +114,18 @@ export default function AuthenticatedLayout({ header, children }) {
                             </div>
 
                             <div className="hidden sm:ms-6 sm:flex sm:items-center">
-                                <div className="relative ms-3">
+                                <div className="flex relative ms-3">
+                                    {user.is_admin && (
+                                        <PrimaryButton
+                                            onClick={(ev) =>
+                                                setShowNewUserModal(true)
+                                            }
+                                        >
+                                            <UserPlusIcon className="h-5 w-5 mr-2" />
+                                            Add New User
+                                        </PrimaryButton>
+                                    )}
+
                                     <Dropdown>
                                         <Dropdown.Trigger>
                                             <span className="inline-flex rounded-md">
@@ -250,6 +265,10 @@ export default function AuthenticatedLayout({ header, children }) {
             </div>
             <Toast />
             <NewMessageNotification />
+            <NewUserModal
+                show={showNewUserModal} 
+                onClose={(ev) => setShowNewUserModal(false)} 
+            />
         </>
     );
 }
